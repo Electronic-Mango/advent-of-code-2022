50987fd993e9fbd133d654b09ae1679a503acb45
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-01 19:08:29 +0100

    Store input files in separate directory, add finals, sort imports

--- src/main/java/aoc2022/day1/Solution1.java
+++ src/main/java/aoc2022/day1/Solution1.java
@@ -1,17 +1,18 @@
 package aoc2022.day1;
 
-import aoc2022.util.Input;
-import org.apache.commons.lang3.math.NumberUtils;
-
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
+import aoc2022.util.Input;
+
 public class Solution1 {
-    private static final String INPUT = "src/main/java/aoc2022/day1/input";
+    private static final String INPUT = "inputs/day1/input";
     private static final String ELF_SEPARATOR = System.lineSeparator() + System.lineSeparator();
     private static final String FOOD_SEPARATOR = System.lineSeparator();
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         final var input = Input.readFile(INPUT);
         final var result = Arrays.stream(input.split(ELF_SEPARATOR))
                 .map(elf -> elf.split(FOOD_SEPARATOR))
--- src/main/java/aoc2022/day1/Solution2.java
+++ src/main/java/aoc2022/day1/Solution2.java
@@ -1,19 +1,20 @@
 package aoc2022.day1;
 
-import aoc2022.util.Input;
-import org.apache.commons.lang3.math.NumberUtils;
-
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.stream.IntStream;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
+import aoc2022.util.Input;
+
 public class Solution2 {
-    private static final String INPUT = "src/main/java/aoc2022/day1/input";
+    private static final String INPUT = "inputs/day1/input";
     private static final String ELF_SEPARATOR = System.lineSeparator() + System.lineSeparator();
     private static final String FOOD_SEPARATOR = System.lineSeparator();
     private static final int ELVES_TO_CALCULATE = 3;
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         final var input = Input.readFile(INPUT);
         final var result = Arrays.stream(input.split(ELF_SEPARATOR))
                 .map(elf -> elf.split(FOOD_SEPARATOR))
