9cba9360b9dc3ca496b38154ea66dbfdbdf31ad5
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-02 12:28:10 +0100

    Add much simpler solutions for day 2

---
+++ src/main/java/aoc2022/day2v2/Solution1.java
@@ -0,0 +1,29 @@
+package aoc2022.day2v2;
+
+import java.util.Arrays;
+
+import aoc2022.util.Input;
+
+public final class Solution1 {
+    private static final String INPUT = "src/main/resources/inputs/day2/input";
+    private static final String SHAPE_SEPARATOR = " ";
+
+    public static void main(final String[] args) {
+        final var input = Input.readLines(INPUT);
+        final var result = input.stream()
+                .map(shapes -> shapes.split(SHAPE_SEPARATOR))
+                .peek(shapes -> System.out.printf(Arrays.toString(shapes) + " "))
+                .mapToInt(Solution1::calculateResult)
+                .peek(System.out::println)
+                .sum();
+        System.out.println(result);
+    }
+
+    private static int calculateResult(final String[] shapes) {
+        final var opponentShape = shapes[0].charAt(0) - 'A';
+        final var myShape = shapes[1].charAt(0) - 'X';
+        final var myShapePoints = myShape + 1;
+        final var resultPoints = Math.floorMod(myShape - opponentShape + 1, 3) * 3;
+        return myShapePoints + resultPoints;
+    }
+}
---
+++ src/main/java/aoc2022/day2v2/Solution2.java
@@ -0,0 +1,25 @@
+package aoc2022.day2v2;
+
+import aoc2022.util.Input;
+
+public final class Solution2 {
+    private static final String INPUT = "src/main/resources/inputs/day2/input";
+    private static final String SHAPE_SEPARATOR = " ";
+
+    public static void main(final String[] args) {
+        final var input = Input.readLines(INPUT);
+        final var result = input.stream()
+                .map(shapes -> shapes.split(SHAPE_SEPARATOR))
+                .mapToInt(Solution2::calculateResult)
+                .sum();
+        System.out.println(result);
+    }
+
+    private static int calculateResult(final String[] shapes) {
+        final var opponentShape = shapes[0].charAt(0) - 'A';
+        final var result = shapes[1].charAt(0) - 'X';
+        final var myShapePoints = Math.floorMod(opponentShape + result - 1, 3) + 1;
+        final var resultPoints = result * 3;
+        return myShapePoints + resultPoints;
+    }
+}
