74255b340a3061f51e6d28f349ef1e3e17f030a6
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-03 13:42:18 +0100

    Add solution for day 3 part 2

---
+++ src/main/java/aoc2022/day3/Solution2.java
@@ -0,0 +1,26 @@
+package aoc2022.day3;
+
+import java.util.Collection;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.primitives.Chars;
+import one.util.streamex.MoreCollectors;
+
+import aoc2022.input.InputLoader;
+
+public final class Solution2 {
+    private static final int GROUP_SIZE = 3;
+
+    public static void main(final String[] args) {
+        final var result = Lists.partition(InputLoader.readLines("day3"), GROUP_SIZE).stream()
+                .map(Collection::stream)
+                .map(contents -> contents.map(String::toCharArray))
+                .map(contents -> contents.map(Chars::asList))
+                .map(contents -> contents.collect(MoreCollectors.intersecting()))
+                .map(Iterables::getOnlyElement)
+                .mapToInt(commonItem -> commonItem - (commonItem < 'a' ? 'A' - 27 : 'a' - 1))
+                .sum();
+        System.out.println(result);
+    }
+}
