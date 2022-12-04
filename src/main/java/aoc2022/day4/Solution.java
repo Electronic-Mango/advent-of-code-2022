17fe25ab03543025e3019b0cd765dcc35ab1a2a4
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-04 13:38:33 +0100

    Make solution for day 4 more generic
    Thanks to StreamEx.ofPairs now each input line can contain multiple ranges, not just two.

--- src/main/java/aoc2022/day4/Solution.java
+++ src/main/java/aoc2022/day4/Solution.java
@@ -6,8 +6,9 @@ import java.util.stream.Stream;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.Range;
+import one.util.streamex.StreamEx;
+import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.math.NumberUtils;
-import org.javatuples.Pair;
 
 import aoc2022.input.InputLoader;
 
@@ -17,34 +18,28 @@ public final class Solution {
 
     public static void main(final String[] args) {
         final var input = InputLoader.readLines("day4");
-        final var resultPart1 = countOccurrences(input, Solution::encloses);
-        final var resultPart2 = countOccurrences(input, Solution::isConnected);
+        final var resultPart1 = countOccurrences(input, Solution::areEnclosed);
+        final var resultPart2 = countOccurrences(input, Solution::areConnected);
         System.out.println(resultPart1);
         System.out.println(resultPart2);
     }
 
-    private static long countOccurrences(final List<String> input,
-                                         final Predicate<Pair<Range<Integer>, Range<Integer>>> filter) {
+    private static long countOccurrences(final List<String> input, final Predicate<List<Range<Integer>>> filter) {
         return input.stream()
                 .map(line -> PAIR_SPLITTER.splitToStream(line)
                         .map(pair -> RANGE_SPLITTER.splitToStream(pair).map(NumberUtils::toInt))
                         .map(Stream::toList)
                         .map(Range::encloseAll))
                 .map(Stream::toList)
-                .map(Pair::fromCollection)
                 .filter(filter)
                 .count();
     }
 
-    private static boolean encloses(final Pair<Range<Integer>, Range<Integer>> ranges) {
-        final var range1 = ranges.getValue0();
-        final var range2 = ranges.getValue1();
-        return range1.encloses(range2) || range2.encloses(range1);
+    private static boolean areEnclosed(final List<Range<Integer>> ranges) {
+        return StreamEx.ofPairs(ranges, (r1, r2) -> r1.encloses(r2) || r2.encloses(r1)).allMatch(BooleanUtils::isTrue);
     }
 
-    private static boolean isConnected(final Pair<Range<Integer>, Range<Integer>> ranges) {
-        final var range1 = ranges.getValue0();
-        final var range2 = ranges.getValue1();
-        return range1.isConnected(range2);
+    private static boolean areConnected(final List<Range<Integer>> ranges) {
+        return StreamEx.ofPairs(ranges, Range::isConnected).allMatch(BooleanUtils::isTrue);
     }
 }
