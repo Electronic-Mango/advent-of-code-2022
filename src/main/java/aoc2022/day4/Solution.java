83955bcef0cd30e5bdd3ea2ca772fe34cdd16fbc
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-04 17:56:21 +0100

    Simplify extracting relevant ranges in day 4

--- src/main/java/aoc2022/day4/Solution.java
+++ src/main/java/aoc2022/day4/Solution.java
@@ -1,7 +1,7 @@
 package aoc2022.day4;
 
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
 import com.google.common.base.Splitter;
@@ -18,28 +18,21 @@ public final class Solution {
 
     public static void main(final String[] args) {
         final var input = InputLoader.readLines("day4");
-        final var resultPart1 = countOccurrences(input, Solution::areEnclosed);
-        final var resultPart2 = countOccurrences(input, Solution::areConnected);
+        final var resultPart1 = countOccurrences(input, (r1, r2) -> r1.encloses(r2) || r2.encloses(r1));
+        final var resultPart2 = countOccurrences(input, Range::isConnected);
         System.out.println(resultPart1);
         System.out.println(resultPart2);
     }
 
-    private static long countOccurrences(final List<String> input, final Predicate<List<Range<Integer>>> filter) {
+    private static long countOccurrences(final List<String> input,
+                                         final BiFunction<Range<Integer>, Range<Integer>, Boolean> mapper) {
         return input.stream()
                 .map(line -> PAIR_SPLITTER.splitToStream(line)
                         .map(pair -> RANGE_SPLITTER.splitToStream(pair).map(NumberUtils::toInt))
                         .map(Stream::toList)
                         .map(Range::encloseAll))
                 .map(Stream::toList)
-                .filter(filter)
+                .filter(ranges -> StreamEx.ofPairs(ranges, mapper).allMatch(BooleanUtils::isTrue))
                 .count();
-    }
-
-    private static boolean areEnclosed(final List<Range<Integer>> ranges) {
-        return StreamEx.ofPairs(ranges, (r1, r2) -> r1.encloses(r2) || r2.encloses(r1)).allMatch(BooleanUtils::isTrue);
-    }
-
-    private static boolean areConnected(final List<Range<Integer>> ranges) {
-        return StreamEx.ofPairs(ranges, Range::isConnected).allMatch(BooleanUtils::isTrue);
     }
 }
