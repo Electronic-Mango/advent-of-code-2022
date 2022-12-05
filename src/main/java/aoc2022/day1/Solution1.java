fc5706b9802133459e8e63e8efe0fae2cf34848e
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-05 19:15:14 +0100

    Use String.lines() rather than splitting on System.lineSeparator()

--- src/main/java/aoc2022/day1/Solution1.java
+++ src/main/java/aoc2022/day1/Solution1.java
@@ -1,25 +1,19 @@
 package aoc2022.day1;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
+import com.google.common.base.Splitter;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import aoc2022.input.InputLoader;
 
 public final class Solution1 {
     private static final String ELF_SEPARATOR = System.lineSeparator() + System.lineSeparator();
-    private static final String FOOD_SEPARATOR = System.lineSeparator();
 
     public static void main(final String[] args) {
         final var input = InputLoader.read("day1");
-        final var result = Arrays.stream(input.split(ELF_SEPARATOR))
-                .map(elf -> elf.split(FOOD_SEPARATOR))
-                .map(Arrays::stream)
-                .map(elf -> elf.mapToInt(NumberUtils::toInt))
-                .mapToInt(IntStream::sum)
+        final var result = Splitter.on(ELF_SEPARATOR).splitToStream(input)
+                .mapToInt(elf -> elf.lines().mapToInt(NumberUtils::toInt).sum())
                 .max()
-                .orElse(0);
+                .orElseThrow();
         System.out.println(result);
     }
 }
--- src/main/java/aoc2022/day1/Solution2.java
+++ src/main/java/aoc2022/day1/Solution2.java
@@ -1,25 +1,20 @@
 package aoc2022.day1;
 
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.stream.IntStream;
 
+import com.google.common.base.Splitter;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import aoc2022.input.InputLoader;
 
 public final class Solution2 {
     private static final String ELF_SEPARATOR = System.lineSeparator() + System.lineSeparator();
-    private static final String FOOD_SEPARATOR = System.lineSeparator();
     private static final int ELVES_TO_CALCULATE = 3;
 
     public static void main(final String[] args) {
         final var input = InputLoader.read("day1");
-        final var result = Arrays.stream(input.split(ELF_SEPARATOR))
-                .map(elf -> elf.split(FOOD_SEPARATOR))
-                .map(Arrays::stream)
-                .map(elf -> elf.mapToInt(NumberUtils::toInt))
-                .map(IntStream::sum)
+        final var result = Splitter.on(ELF_SEPARATOR).splitToStream(input)
+                .map(elf -> elf.lines().mapToInt(NumberUtils::toInt).sum())
                 .sorted(Comparator.reverseOrder())
                 .limit(ELVES_TO_CALCULATE)
                 .mapToInt(Integer::intValue)
