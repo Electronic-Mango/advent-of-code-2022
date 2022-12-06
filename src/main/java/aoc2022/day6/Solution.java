73f9ca2ed97a4b87d324113847cbca5efcc952fc
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-06 08:45:43 +0100

    Add solutions for both parts for day 6

---
+++ src/main/java/aoc2022/day6/Solution.java
@@ -0,0 +1,29 @@
+package aoc2022.day6;
+
+import java.util.List;
+import java.util.Set;
+
+import one.util.streamex.StreamEx;
+
+import aoc2022.input.InputLoader;
+
+public final class Solution {
+    private static final int START_OF_PACKET_SIZE_1 = 4;
+    private static final int START_OF_PACKET_SIZE_2 = 14;
+
+    public static void main(final String[] args) {
+        final var buffer = InputLoader.read("day6").strip().chars().boxed().toList();
+        final var startPacket1 = calculateStartOfPacketIndex(buffer, START_OF_PACKET_SIZE_1);
+        final var startPacket2 = calculateStartOfPacketIndex(buffer, START_OF_PACKET_SIZE_2);
+        System.out.println(startPacket1);
+        System.out.println(startPacket2);
+    }
+
+    private static long calculateStartOfPacketIndex(final List<Integer> buffer, final int startOfPacketSize) {
+        return StreamEx.ofSubLists(buffer, startOfPacketSize, 1)
+                .map(Set::copyOf)
+                .map(Set::size)
+                .indexOf(size -> size == startOfPacketSize)
+                .orElseThrow() + startOfPacketSize;
+    }
+}
