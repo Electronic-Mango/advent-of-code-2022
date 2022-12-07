64e36e7ba1c3d4bb1f9815499057a2dda6c4cf5c
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-07 11:26:15 +0100

    Simplify solution for day 7 even further

--- src/main/java/aoc2022/day7/Solution.java
+++ src/main/java/aoc2022/day7/Solution.java
@@ -1,71 +1,50 @@
 package aoc2022.day7;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.google.common.base.Splitter;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import one.util.streamex.IntStreamEx;
+import one.util.streamex.StreamEx;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import aoc2022.input.InputLoader;
 
 public final class Solution {
+    private static final int MAX_DIR_SIZE = 100_000;
     private static final int TOTAL_MEMORY = 70_000_000;
     private static final int REQUIRED_MEMORY = 30_000_000;
 
     public static void main(final String[] args) {
-        final var input = InputLoader.readLines("day7", "input");
-        final var fileTree = new LinkedHashMap<String, Directory>();
-        final var rootDir = new Directory("/", null);
-        fileTree.put(rootDir.getName(), rootDir);
-        var currentDir = rootDir;
+        final var input = InputLoader.readLines("day7").stream().skip(1).toList();
+        final var rootDirectory = new Directory(null);
+        var currentDirectory = rootDirectory;
         for (final var command : input) {
-            final var commandParameters = Splitter.on(' ').splitToList(command);
-            if (commandParameters.get(0).equals("$")) {
-                if (commandParameters.get(1).equals("cd")) {
-                    final var dirName = commandParameters.get(2);
-                    if (!dirName.equals("..")) {
-                        final var newDir = new Directory(dirName, currentDir);
-                        if (fileTree.containsKey(newDir.getFullPath())) {
-                            currentDir = fileTree.get(newDir.getFullPath());
-                        } else {
-                            fileTree.put(newDir.getFullPath(), newDir);
-                            currentDir.addSubDirectory(newDir);
-                            currentDir = newDir;
-                        }
-                    } else {
-                        currentDir = currentDir.getParent();
-                    }
-                }
+            if (command.contains("$ cd ..")) {
+                currentDirectory = currentDirectory.getParent();
+            } else if (command.contains("$ cd")) {
+                final var directory = new Directory(currentDirectory);
+                currentDirectory.addSubDirectory(directory);
+                currentDirectory = directory;
             } else {
-                if (NumberUtils.isParsable(commandParameters.get(0))) {
-                    currentDir.addFile(NumberUtils.toInt(commandParameters.get(0)));
-                }
+                currentDirectory.addFile(NumberUtils.toInt(command.replaceAll("\\D+", "")));
             }
         }
-        final var result1 = fileTree.values().stream().filter(dir -> dir.getFullSize() <= 100000)
-                .mapToInt(Directory::getFullSize).sum();
-        System.out.println("result=" + result1 + " expected=1086293 matches=" + (result1 == 1086293));
-        final var freeMemory = TOTAL_MEMORY - rootDir.getFullSize();
-        final var missingMemory = REQUIRED_MEMORY - freeMemory;
-        final var result2 = fileTree.values().stream().filter(dir -> dir.getFullSize() > missingMemory)
-                .mapToInt(Directory::getFullSize).min().orElseThrow();
-        System.out.println("result=" + result2 + " expected=366028 matches=" + (result2 == 366028));
+        final var result1 = rootDirectory.flatSizesStream().filter(size -> size <= MAX_DIR_SIZE).sum();
+        System.out.println(result1);
+        final var missingMemory = REQUIRED_MEMORY - (TOTAL_MEMORY - rootDirectory.getFullSize());
+        final var result2 = rootDirectory.flatSizesStream().filter(size -> size > missingMemory).min().orElseThrow();
+        System.out.println(result2);
     }
 }
 
 @Getter
 @RequiredArgsConstructor
-//@ToString
 class Directory {
-    private final String name;
     private final Directory parent;
     private final List<Directory> subDirectories = new ArrayList<>();
     private int directSize = 0;
-
-    public String getFullPath() {
-        return parent == null ? name : parent.getFullPath() + name + "/";
-    }
 
     public int getFullSize() {
         return directSize + subDirectories.stream().mapToInt(Directory::getFullSize).sum();
@@ -77,5 +56,13 @@ class Directory {
 
     public void addFile(final int size) {
         directSize += size;
+    }
+
+    public IntStreamEx flatSizesStream() {
+        return StreamEx.of(subDirectories)
+                .map(Directory::flatSizesStream)
+                .flatMap(IntStreamEx::boxed)
+                .append(getFullSize())
+                .mapToInt(Integer::valueOf);
     }
 }
