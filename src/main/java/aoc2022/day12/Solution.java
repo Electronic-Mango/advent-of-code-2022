a5bdcbf3383709a0b53d977897c6d3960d956939
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-12 18:26:36 +0100

    Tweak day 12, calculate all distances once and extract relevant data

--- src/main/java/aoc2022/day12/Solution.java
+++ src/main/java/aoc2022/day12/Solution.java
@@ -1,22 +1,25 @@
 package aoc2022.day12;
 
 import aoc2022.input.InputLoader;
+import com.google.common.collect.Lists;
 import one.util.streamex.EntryStream;
 import one.util.streamex.IntStreamEx;
 import one.util.streamex.StreamEx;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.awt.Point;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public final class Solution {
-    private static final Integer START = (int) 'S';
-    private static final Integer END = (int) 'E';
-    private static final Integer LOWEST_HEIGHT = (int) 'a';
-    private static final Integer TALLEST_HEIGHT = (int) 'z';
+    private static final int START_VALUE = 'S';
+    private static final int END_VALUE = 'E';
+    private static final int LOWEST_HEIGHT = 'a';
+    private static final int TALLEST_HEIGHT = 'z';
 
     public static void main(final String[] args) {
         final var input = InputLoader.readLines("day12");
@@ -27,45 +30,45 @@ public final class Solution {
                 .flatMapToKey((y, row) -> IntStreamEx.range(row.size()).mapToObj(x -> new Point(x, y)))
                 .mapToValue((point, row) -> row.get(point.x))
                 .toMap();
-        final var startPoint = EntryStream.of(heightMap).filterValues(START::equals).keys().findAny().orElseThrow();
-        final var endPoint = EntryStream.of(heightMap).filterValues(END::equals).keys().findAny().orElseThrow();
-        heightMap.put(startPoint, LOWEST_HEIGHT);
-        heightMap.put(endPoint, TALLEST_HEIGHT);
+        final var start = getPointWithHeight(heightMap, START_VALUE);
+        final var end = getPointWithHeight(heightMap, END_VALUE);
+        heightMap.put(start, LOWEST_HEIGHT);
+        heightMap.put(end, TALLEST_HEIGHT);
 
-        final var result1 = solve(heightMap, startPoint, endPoint);
-        System.out.println(result1);
-        System.out.println();
-        final var result2 = EntryStream.of(heightMap)
-                .filterValues(LOWEST_HEIGHT::equals)
-                .keys()
-                .mapToInt(start -> solve(heightMap, start, endPoint))
+        final var distances = getDistances(heightMap, end);
+        final var result1 = distances.get(start);
+        final var result2 = EntryStream.of(distances)
+                .filterKeys(point -> heightMap.get(point).equals(LOWEST_HEIGHT))
+                .values()
+                .mapToInt(Integer::valueOf)
                 .min()
                 .orElseThrow();
+        System.out.println(result1);
         System.out.println(result2);
     }
 
-    private static int solve(final Map<Point, Integer> heightMap, final Point start, final Point end) {
-        final var visitedPoints = new HashMap<>(Map.of(start, 0, end, Integer.MAX_VALUE));
-        final var handledPoints = new HashSet<Point>();
-        int handledPointsSizeStart;
-        do {
-            handledPointsSizeStart = handledPoints.size();
-            final var pointsToHandle = StreamEx.of(visitedPoints.keySet())
-                    .filterBy(end::equals, false)
-                    .filterBy(handledPoints::contains, false)
-                    .toList();
-            for (final var point : pointsToHandle) {
-                handledPoints.add(point);
-                final var height = heightMap.get(point) + 1;
-                final var steps = visitedPoints.get(point) + 1;
-                StreamEx.of(new Point(-1, 0), new Point(1, 0), new Point(0, -1), new Point(0, 1))
-                        .map(delta -> new Point(point.x - delta.x, point.y - delta.y))
-                        .filter(heightMap::containsKey)
-                        .filter(next -> heightMap.get(next) <= height)
-                        .filter(next -> visitedPoints.get(next) == null || visitedPoints.get(next) > steps)
-                        .forEach(next -> visitedPoints.put(next, steps));
-            }
-        } while (visitedPoints.get(end) == Integer.MAX_VALUE && handledPoints.size() > handledPointsSizeStart);
-        return visitedPoints.get(end);
+    private static Point getPointWithHeight(final Map<Point, Integer> heightMap, final int target) {
+        return EntryStream.of(heightMap).filterValues(height -> height.equals(target)).keys().findAny().orElseThrow();
+    }
+
+    private static Map<Point, Integer> getDistances(final Map<Point, Integer> heightMap, final Point start) {
+        final var distances = new HashMap<Point, Integer>();
+        final var pointsToHandle = new LinkedList<Pair<Point, Integer>>();
+        pointsToHandle.add(Pair.of(start, 0));
+        while (!pointsToHandle.isEmpty()) {
+            final var state = pointsToHandle.pollFirst();
+            final var nextSteps = state.getRight() + 1;
+            final var position = state.getLeft();
+            StreamEx.of(Pair.of(-1, 0), Pair.of(1, 0), Pair.of(0, -1), Pair.of(0, 1))
+                    .map(delta -> new Point(position.x - delta.getLeft(), position.y - delta.getRight()))
+                    .filter(heightMap::containsKey)
+                    .filterBy(distances::containsKey, false)
+                    .filter(point -> heightMap.get(point) + 1 >= heightMap.get(position))
+                    .forEach(point -> {
+                        pointsToHandle.addLast(Pair.of(point, nextSteps));
+                        distances.put(point, nextSteps);
+                    });
+        }
+        return distances;
     }
 }
