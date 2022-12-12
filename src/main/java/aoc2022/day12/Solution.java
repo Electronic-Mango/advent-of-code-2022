13db8748002347b51c52004ec29944eb441676d1
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-12 12:23:20 +0100

    Tweak day 12, use flat structure for holding heights

--- src/main/java/aoc2022/day12/Solution.java
+++ src/main/java/aoc2022/day12/Solution.java
@@ -1,128 +1,124 @@
 package aoc2022.day12;
 
 import aoc2022.input.InputLoader;
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import one.util.streamex.EntryStream;
-import one.util.streamex.MoreCollectors;
-import org.electronicmango.zipper.Zipper;
 
-import java.awt.Point;
+import java.awt.*;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+//370
+//363
 public final class Solution {
     private static final int START_LOCATION = 'S';
     private static final int END_LOCATION = 'E';
     private static final int LOWEST_HEIGHT = 'a';
     private static final int TALLEST_HEIGHT = 'z';
 
-    private static int ITER = 1;
-
     public static void main(final String[] args) {
-        final var input = InputLoader.readLines("day12").stream()
-                .map(String::chars)
-                .map(IntStream::boxed)
-                .map(Stream::toList)
-                .collect(Zipper.zipCollector());
-        final var startPoint = EntryStream.of(input)
-                .mapValues(row -> row.indexOf(START_LOCATION))
-                .filterValues(y -> y != -1)
-                .mapKeyValue(Point::new)
-                .collect(MoreCollectors.onlyOne())
-                .orElseThrow();
-        final var endPoint = EntryStream.of(input)
-                .mapValues(row -> row.indexOf(END_LOCATION))
-                .filterValues(y -> y != -1)
-                .mapKeyValue(Point::new)
-                .collect(MoreCollectors.onlyOne())
-                .orElseThrow();
+        final var input = InputLoader.readLines("day12");
+        final var heightMap = EntryStream.of(input)
+                .mapValues(String::chars)
+                .mapValues(IntStream::boxed)
+                .mapValues(Stream::toList)
+                .flatMapValues(EntryStream::of)
+                .mapToKey((y, xHeightEntry) -> new Point(xHeightEntry.getKey(), y))
+                .mapValues(Map.Entry::getValue)
+                .toMap();
+        final var startPoint = getPointWithHeight(heightMap, START_LOCATION);
+        final var endPoint = getPointWithHeight(heightMap, END_LOCATION);
+        heightMap.put(startPoint, LOWEST_HEIGHT);
+        heightMap.put(endPoint, TALLEST_HEIGHT);
         State.setEndPoint(endPoint);
-        EntryStream.of(input)
-                .filterKeys(x -> x == startPoint.x)
-                .values()
-                .forEach(column -> column.set(startPoint.y, LOWEST_HEIGHT));
-        EntryStream.of(input)
-                .filterKeys(x -> x == endPoint.x)
-                .values()
-                .forEach(column -> column.set(endPoint.y, TALLEST_HEIGHT));
-        final var result1 = solve(input, startPoint);
+
+        final var result1 = solve(heightMap, startPoint);
         System.out.println(result1);
         System.out.println();
-        final var result2 = EntryStream.of(input)
-                .flatMapValues(EntryStream::of)
-                .filterValues(row -> row.getValue() == LOWEST_HEIGHT)
-                .mapValues(Map.Entry::getKey)
-                .mapKeyValue(Point::new)
-                .mapToInt(start -> solve(input, start))
+        final var result2 = EntryStream.of(heightMap)
+                .filterValues(height -> height == LOWEST_HEIGHT)
+                .keys()
+                .mapToInt(start -> solve(heightMap, start))
+                .peek(System.out::println)
                 .min()
                 .orElseThrow();
         System.out.println(result2);
     }
 
-    private static int solve(final List<List<Integer>> grid, final Point startPoint) {
+    private static Point getPointWithHeight(final Map<Point, Integer> heightMap, final int targetHeight) {
+        return EntryStream.of(heightMap).filterValues(height -> height == targetHeight).keys().findAny().orElseThrow();
+    }
+
+    private static int solve(final Map<Point, Integer> heightMap, final Point startPoint) {
         final var visitedLocations = new HashSet<State>();
-        visitedLocations.add(new State(startPoint, 0));
+        visitedLocations.add(new State(startPoint));
         while (!visitedLocations.stream().allMatch(State::isDone)) {
-            for (final var state : new HashSet<>(visitedLocations)) {
-                if (state.isDone() || state.isFinished()) {
-                    continue;
-                }
+            for (final var state : visitedLocations.stream().filter(state -> !state.isDone()).toList()) {
                 final var position = state.getPosition();
                 final var steps = state.getSteps();
-                final var height = (char) grid.get(position.x).get(position.y).intValue();
-                final var deltas = List.of(List.of(-1, 0), List.of(1, 0), List.of(0, -1), List.of(0, 1));
-                for (final var delta : deltas) {
-                    final var newX = position.x + delta.get(0);
-                    final var newY = position.y + delta.get(1);
-                    if (newX < 0 || newY < 0 || newX >= grid.size() || newY >= grid.get(newX).size()) {
+                final var height = heightMap.get(position);
+                final var neighbouringPoints = List.of(
+                        new Point(position.x - 1, position.y),
+                        new Point(position.x + 1, position.y),
+                        new Point(position.x, position.y - 1),
+                        new Point(position.x, position.y + 1)
+                );
+                for (final var neighbour : neighbouringPoints.stream().filter(heightMap::containsKey).toList()) {
+                    final var neighbourHeight = heightMap.get(neighbour);
+                    if (neighbourHeight > (height + 1)) {
                         continue;
                     }
-                    final var newHeight = (char) grid.get(newX).get(newY).intValue();
-                    if (newHeight > (height + 1)) {
-                        continue;
-                    }
-                    final var newState = new State(new Point(newX, newY), steps + 1);
-                    if (newState.isFinished()) {
-                        newState.setDone(true);
-                        visitedLocations.add(newState);
-                        continue;
-                    }
+                    final var newState = new State(neighbour, steps + 1);
                     final var differentRoutes = visitedLocations.stream()
                             .filter(s -> s.isHere(newState.getPosition()))
                             .toList();
+                    var bestRoute = true;
                     for (final var differentRoute : differentRoutes) {
                         if (differentRoute.getSteps() > newState.getSteps()) {
                             differentRoute.setDone(true);
                         } else {
-                            newState.setDone(true);
+                            bestRoute = false;
                         }
                     }
-                    if (!newState.isDone()) {
+                    if (bestRoute) {
                         visitedLocations.add(newState);
                     }
                 }
                 state.setDone(true);
             }
         }
-        final var result = visitedLocations.stream()
+        return visitedLocations.stream()
                 .filter(State::isFinished)
                 .mapToInt(State::getSteps)
                 .min()
                 .orElse(Integer.MAX_VALUE);
-        System.out.println(ITER++ + " startState=" + startPoint + " result=" + result);
-        return result;
     }
 }
 
-@Data
+@Getter(AccessLevel.PACKAGE)
+@Setter(AccessLevel.PACKAGE)
+@EqualsAndHashCode
 final class State {
     private static Point END_POINT = new Point(0, 0);
     private final Point position;
     private final int steps;
-    private boolean done = false;
+    private boolean done;
+
+    State(final Point position) {
+        this(position, 0);
+    }
+
+    State(final Point position, final int steps) {
+        this.position = position;
+        this.steps = steps;
+        this.done = isFinished();
+    }
 
     static void setEndPoint(final Point endPoint) {
         END_POINT = endPoint;
