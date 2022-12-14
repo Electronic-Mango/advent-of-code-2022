6d9a0fa5d319b8fc869582cd6326ab3353750a2b
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-14 20:48:36 +0100

    Simplify day 14 solution
    Run simulation fully recursively.
    Store map as a set of occupied points.

--- src/main/java/aoc2022/day14/Solution.java
+++ src/main/java/aoc2022/day14/Solution.java
@@ -1,91 +1,68 @@
 package aoc2022.day14;
 
 import aoc2022.input.InputLoader;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Iterators;
+import com.google.common.collect.Lists;
 import one.util.streamex.IntStreamEx;
 import one.util.streamex.StreamEx;
+import org.javatuples.Pair;
 
 import java.awt.Point;
-import java.awt.geom.Line2D;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public final class Solution {
+    private static final Point START = new Point(500, 0);
     public static void main(final String[] args) {
         final var input = InputLoader.readLines("day14");
-        final var lines = StreamEx.of(input)
-                .map(line -> line.split(" -> "))
-                .map(Arrays::asList)
-                .flatMap(points -> StreamEx.of(points)
-                        .map(point -> point.split(","))
-                        .map(Arrays::asList)
-                        .map(point -> point.stream().map(Integer::parseInt).toList())
+        final var occupiedPoints = StreamEx.of(input)
+                .map(line -> StreamEx.split(line, " -> "))
+                .flatMap(points -> points
+                        .map(point -> StreamEx.split(point, ",").map(Integer::parseInt).toList())
                         .map(point -> new Point(point.get(0), point.get(1)))
-                        .pairMap(Line2D.Double::new))
-                .toMutableList();
-        final var xStats = lines.stream()
-                .flatMap(line -> Stream.of(line.x1, line.x2))
-                .mapToInt(Double::intValue)
-                .summaryStatistics();
-        final var minX = xStats.getMin() - 1000;
-        final var maxX = xStats.getMax() + 1000;
-        final var maxY = lines.stream()
-                .flatMap(line -> Stream.of(line.y1, line.y2))
-                .mapToInt(Double::intValue)
-                .max()
-                .orElseThrow();
-        final var sandStartLocation = 500 - minX;
-        final var grid1 = prepareGrid(lines, maxY, minX, maxX);
-        var count1 = 0;
-        while (!simulateSand(grid1, sandStartLocation, 0)) {
-            count1++;
-        }
-        System.out.println(count1);
+                        .pairMap(Pair::with))
+                .flatMap(line -> expandLine(line.getValue0(), line.getValue1()))
+                .toMutableSet();
+        final var maxY = occupiedPoints.stream().map(Point::getY).mapToInt(Double::intValue).max().orElseThrow();
+        final var bottomLine = expandLine(new Point(START.x - 2 * maxY, maxY + 2), new Point(START.x + 2 * maxY, maxY + 2));
+        occupiedPoints.addAll(bottomLine.toList());
 
-        lines.add(new Line2D.Double(minX, maxY + 2, maxX, maxY + 2));
-        final var grid2 = prepareGrid(lines, maxY, minX, maxX);
-        var count2 = 0;
+        var count = 0;
+        while (simulate(occupiedPoints, START, endPoint -> endPoint.y > maxY)) {
+            count++;
+        }
+        System.out.println(count);
+
         do {
-            count2++;
-        } while (!simulateSand(grid2, sandStartLocation, 0));
-        System.out.println(count2);
+            count++;
+        } while (simulate(occupiedPoints, START, START::equals));
+        System.out.println(count + 1);
     }
 
-    private static List<List<Character>> prepareGrid(final List<Line2D.Double> lines,
-                                                     final int maxY,
-                                                     final int minX,
-                                                     final int maxX) {
-        final var grid = IntStreamEx.range(maxY + 2)
-                .mapToObj(y -> IntStreamEx.generate(() -> ' ')
-                        .limit(maxX - minX + 1)
-                        .mapToObj(i -> (char) i)
-                        .toMutableList())
-                .toMutableList();
-        for (final var line : lines) {
-            for (double x = Math.min(line.x1, line.x2); x <= Math.max(line.x1, line.x2); ++x) {
-                for (double y = Math.min(line.y1, line.y2); y <= Math.max(line.y1, line.y2); ++y) {
-                    grid.get((int) y - 1).set((int) x - minX, '#');
-                }
-            }
-        }
-        return grid;
+    private static Stream<Point> expandLine(final Point start, final Point end) {
+        final var span = Math.max(Math.abs(start.x - end.x), Math.abs(start.y - end.y));
+        final var xSpan = range(start.x, end.x, span);
+        final var ySpan = range(start.y, end.y, span);
+        return StreamEx.zip(xSpan, ySpan, Point::new);
     }
 
-    private static boolean simulateSand(final List<List<Character>> grid, int startX, int startY) {
-        for (int y = startY; y < grid.size(); ++y) {
-            final var cell = grid.get(y).get(startX);
-            if (cell == ' ') {
-                continue;
-            } else if (grid.get(y).get(startX - 1) == ' ') {
-                return simulateSand(grid, startX - 1, y + 1);
-            } else if (grid.get(y).get(startX + 1) == ' ') {
-                return simulateSand(grid, startX + 1, y + 1);
-            } else if (y > 0) {
-                grid.get(y - 1).set(startX, 'o');
-                return false;
-            }
-            return true;
+    private static List<Integer> range(final int p1, final int p2, final int span) {
+        final var cycle = IntStreamEx.rangeClosed(Math.min(p1, p2), Math.max(p1, p2)).boxed().toList();
+        return Lists.newArrayList(Iterators.limit(Iterables.cycle(cycle).iterator(), span + 1));
+    }
+
+    private static boolean simulate(final Set<Point> occupied, final Point pos, final Function<Point, Boolean> cond) {
+        if (!occupied.contains(new Point(pos.x, pos.y + 1))) {
+            return simulate(occupied, new Point(pos.x, pos.y + 1), cond);
+        } else if (!occupied.contains(new Point(pos.x - 1, pos.y + 1))) {
+            return simulate(occupied, new Point(pos.x - 1, pos.y + 1), cond);
+        } else if (!occupied.contains(new Point(pos.x + 1, pos.y + 1))) {
+            return simulate(occupied, new Point(pos.x + 1, pos.y + 1), cond);
+        } else {
+            occupied.add(pos);
+            return !cond.apply(pos);
         }
-        return true;
     }
 }
