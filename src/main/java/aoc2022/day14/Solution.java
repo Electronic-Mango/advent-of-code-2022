f73b131d5df737fc824d375262c6dfe6a7a2282a
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-14 21:36:35 +0100

    Simplify day 14 solution
    Use multimap for storing occupied points.
    Simulate sand using separate x and y coordinates, rather than compound point.

--- src/main/java/aoc2022/day14/Solution.java
+++ src/main/java/aoc2022/day14/Solution.java
@@ -3,66 +3,71 @@ package aoc2022.day14;
 import aoc2022.input.InputLoader;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Iterators;
+import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
 import one.util.streamex.IntStreamEx;
 import one.util.streamex.StreamEx;
+import org.apache.commons.lang3.function.ToBooleanBiFunction;
 import org.javatuples.Pair;
 
 import java.awt.Point;
-import java.util.*;
-import java.util.function.Function;
+import java.util.List;
 import java.util.stream.Stream;
 
 public final class Solution {
-    private static final Point START = new Point(500, 0);
+    private static final int FLOOR_DISTANCE = 2;
+    private static final int SAND_START_X = 500;
+    private static final int SAND_START_Y = 0;
+
     public static void main(final String[] args) {
-        final var input = InputLoader.readLines("day14");
-        final var occupiedPoints = StreamEx.of(input)
-                .map(line -> StreamEx.split(line, " -> "))
-                .flatMap(points -> points
+        final var occupied = LinkedHashMultimap.<Integer, Integer>create();
+        InputLoader.readLines("day14").stream()
+                .flatMap(line -> StreamEx.split(line, " -> ")
                         .map(point -> StreamEx.split(point, ",").map(Integer::parseInt).toList())
-                        .map(point -> new Point(point.get(0), point.get(1)))
-                        .pairMap(Pair::with))
-                .flatMap(line -> expandLine(line.getValue0(), line.getValue1()))
-                .toMutableSet();
-        final var maxY = occupiedPoints.stream().map(Point::getY).mapToInt(Double::intValue).max().orElseThrow();
-        final var bottomLine = expandLine(new Point(START.x - 2 * maxY, maxY + 2), new Point(START.x + 2 * maxY, maxY + 2));
-        occupiedPoints.addAll(bottomLine.toList());
+                        .map(Pair::fromCollection)
+                        .pairMap(Pair::add))
+                .flatMap(line -> expandLine(line.getValue0(), line.getValue1(), line.getValue2(), line.getValue3()))
+                .forEach(point -> occupied.put(point.getValue0(), point.getValue1()));
+        final var height = occupied.values().stream().mapToInt(Integer::valueOf).max().orElseThrow() + FLOOR_DISTANCE;
+        expandLine(SAND_START_X - height, height, SAND_START_X + height, height)
+                .forEach(point -> occupied.put(point.getValue0(), point.getValue1()));
 
         var count = 0;
-        while (simulate(occupiedPoints, START, endPoint -> endPoint.y > maxY)) {
+        while (simulate(occupied, (x, y) -> y <= height - FLOOR_DISTANCE, SAND_START_X, SAND_START_Y)) {
             count++;
         }
         System.out.println(count);
 
         do {
             count++;
-        } while (simulate(occupiedPoints, START, START::equals));
+        } while (simulate(occupied, (x, y) -> x != SAND_START_X || y != SAND_START_Y, SAND_START_X, SAND_START_Y));
         System.out.println(count + 1);
     }
 
-    private static Stream<Point> expandLine(final Point start, final Point end) {
-        final var span = Math.max(Math.abs(start.x - end.x), Math.abs(start.y - end.y));
-        final var xSpan = range(start.x, end.x, span);
-        final var ySpan = range(start.y, end.y, span);
-        return StreamEx.zip(xSpan, ySpan, Point::new);
+    private static Stream<Pair<Integer, Integer>> expandLine(final int x1, final int y1, final int x2, final int y2) {
+        final var span = (int) Point.distance(x1, y1, x2, y2);
+        return StreamEx.zip(range(x1, x2, span), range(y1, y2, span), Pair::with);
     }
 
     private static List<Integer> range(final int p1, final int p2, final int span) {
-        final var cycle = IntStreamEx.rangeClosed(Math.min(p1, p2), Math.max(p1, p2)).boxed().toList();
-        return Lists.newArrayList(Iterators.limit(Iterables.cycle(cycle).iterator(), span + 1));
+        final var cyclicIterator = IntStreamEx.rangeClosed(Math.min(p1, p2), Math.max(p1, p2))
+                .boxed().toListAndThen(Iterables::cycle).iterator();
+        return Lists.newArrayList(Iterators.limit(cyclicIterator, span + 1));
     }
 
-    private static boolean simulate(final Set<Point> occupied, final Point pos, final Function<Point, Boolean> cond) {
-        if (!occupied.contains(new Point(pos.x, pos.y + 1))) {
-            return simulate(occupied, new Point(pos.x, pos.y + 1), cond);
-        } else if (!occupied.contains(new Point(pos.x - 1, pos.y + 1))) {
-            return simulate(occupied, new Point(pos.x - 1, pos.y + 1), cond);
-        } else if (!occupied.contains(new Point(pos.x + 1, pos.y + 1))) {
-            return simulate(occupied, new Point(pos.x + 1, pos.y + 1), cond);
-        } else {
-            occupied.add(pos);
-            return !cond.apply(pos);
+    private static boolean simulate(final Multimap<Integer, Integer> occupied,
+                                    final ToBooleanBiFunction<Integer, Integer> restPointCondition,
+                                    final int x,
+                                    final int y) {
+        if (!occupied.containsEntry(x, y + 1)) {
+            return simulate(occupied, restPointCondition, x, y + 1);
+        } else if (!occupied.containsEntry(x - 1, y + 1)) {
+            return simulate(occupied, restPointCondition, x - 1, y + 1);
+        } else if (!occupied.containsEntry(x + 1, y + 1)) {
+            return simulate(occupied, restPointCondition, x + 1, y + 1);
         }
+        occupied.put(x, y);
+        return restPointCondition.applyAsBoolean(x, y);
     }
 }
