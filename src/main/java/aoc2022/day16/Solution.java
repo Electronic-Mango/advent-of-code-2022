37e97eed9832c84d32f52b42c9fc5be3c2023c63
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-16 20:14:40 +0100

    Streamline day 16

--- src/main/java/aoc2022/day16/Solution.java
+++ src/main/java/aoc2022/day16/Solution.java
@@ -4,87 +4,71 @@ import aoc2022.input.InputLoader;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Sets;
 import com.google.common.collect.Table;
-import lombok.Data;
 import one.util.streamex.EntryStream;
 import one.util.streamex.StreamEx;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.paukov.combinatorics3.Generator;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.regex.Matcher;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 
 public final class Solution {
+    private static final Pattern VALVE_PATTERN = Pattern.compile("Valve ([A-Z][A-Z]).+rate=(\\d+);.*valves? (.+)");
+    private static final Map<String, Integer> RATES = new HashMap<>();
+    private static final Table<String, String, Integer> DISTANCES = HashBasedTable.create();
+    private static final String INITIAL_VALVE = "AA";
     private static final int MINUTES_ALONE = 30;
     private static final int MINUTES_TOGETHER = 26;
-    private static final String INITIAL_VALVE = "AA";
-    private static final Pattern VALVE_PATTERN =
-            Pattern.compile("^Valve ([A-Z][A-Z]) .+rate=(\\d+);.*valves? ([A-Z ,]+)$");
-    private static final Set<String> V = new HashSet<>();
-    private static final Map<String, Long> F = new HashMap<>();
-    private static final Table<String, String, Long> D = HashBasedTable.create();
 
     public static void main(String[] args) {
-        final var ts = System.nanoTime();
-        InputLoader.readLines("day16", "testinput")
-                .stream()
-                .map(VALVE_PATTERN::matcher)
-                .flatMap(Matcher::results)
-                .map(r -> new Input(r.group(1), r.group(2), r.group(3)))
-                .forEach(input -> {
-                    V.add(input.getName());
-                    F.put(input.getName(), input.getRate());
-                    input.getTargets().forEach(t -> D.put(t, input.getName(), 1L));
-                });
-        EntryStream.of(new HashMap<>(F)).filterValues(d -> d == 0).keys().forEach(F::remove);
-        final var count = new AtomicInteger(0);
-        Generator.permutation(V).withRepetitions(3).forEach(p -> {
-            final var k = p.get(2);
-            final var i = p.get(1);
-            final var j = p.get(0);
-            final var v1 = D.contains(i, j) ? D.get(i, j) : Integer.MAX_VALUE;
-            final var v2 = D.contains(i, k) ? D.get(i, k) : Integer.MAX_VALUE;
-            final var v3 = D.contains(k, j) ? D.get(k, j) : Integer.MAX_VALUE;
-            final var v = Math.min(v1, v2 + v3);
-            D.put(j, i, v);
-            count.incrementAndGet();
+        VALVE_PATTERN.matcher(InputLoader.read("day16")).results().forEach(match -> {
+            RATES.put(match.group(1), NumberUtils.toInt(match.group(2)));
+            StreamEx.split(match.group(3), ", ").forEach(next -> DISTANCES.put(match.group(1), next, 1));
         });
-        System.out.println(search(MINUTES_ALONE, INITIAL_VALVE, F.keySet(), false));
-        System.out.println(search(MINUTES_TOGETHER, INITIAL_VALVE, F.keySet(), true));
-        final var te = System.nanoTime();
-        System.out.println(te - ts);
+        Generator.permutation(RATES.keySet()).withRepetitions(3).forEach(Solution::populateDistances);
+        EntryStream.of(Map.copyOf(RATES)).filterValues(d -> d == 0).keys().forEach(RATES::remove);
+
+        System.out.println(search(MINUTES_ALONE, INITIAL_VALVE, RATES.keySet(), false));
+        System.out.println(search(MINUTES_TOGETHER, INITIAL_VALVE, RATES.keySet(), true));
     }
 
-    private static long search(final long t, final String u, final Set<String> vs, final boolean together) {
-        return StreamEx.of(vs)
+    private static void populateDistances(final List<String> permutation) {
+        final var start = permutation.get(0);
+        final var end = permutation.get(1);
+        final var through = permutation.get(2);
+        final var directDistance = distance(start, end);
+        final var indirectDistance = distance(end, through) + distance(through, start);
+        DISTANCES.put(start, end, Math.min(directDistance, indirectDistance));
+    }
+
+    private static int search(final int time, final String current, final Set<String> next, final boolean together) {
+        return StreamEx.of(next)
+                .unordered()
                 .parallel()
-                .filter(v -> D.get(u, v) < t)
-                .mapToLong(v -> nextIteration(v, t - D.get(u, v) - 1, Sets.newHashSet(vs), together))
-                .append(together ? search(MINUTES_TOGETHER, INITIAL_VALVE, vs, false) : 0)
+                .filter(valve -> distance(current, valve) < time)
+                .mapToEntry(Function.identity(), Function.identity())
+                .mapKeys(valve -> RATES.get(valve) * timeLeft(time, current, valve))
+                .mapValues(valve -> search(timeLeft(time, current, valve), valve, remove(next, valve), together))
+                .mapKeyValue(Math::addExact)
+                .mapToInt(Integer::valueOf)
+                .append(together ? search(MINUTES_TOGETHER, INITIAL_VALVE, next, false) : 0)
                 .max()
                 .orElse(0);
     }
 
-    private static long nextIteration(final String v, final long tr, final Set<String> vs, final boolean together) {
-        vs.remove(v);
-        return F.get(v) * tr + search(tr, v, vs, together);
+    private static int timeLeft(final int time, final String currentValve, final String nextValve) {
+        return time - distance(currentValve, nextValve) - 1;
     }
 
-    @Data
-    private static final class Input {
-        private final String name;
-        private final long rate;
-        private final List<String> targets;
+    private static Set<String> remove(final Set<String> set, final String value) {
+        return Sets.difference(set, Set.of(value));
+    }
 
-        Input(final String name, final String rate, final String targets) {
-            this.name = name;
-            this.rate = NumberUtils.toLong(rate);
-            this.targets = StreamEx.split(targets, ", ").toList();
-        }
+    private static int distance(final String a, final String b) {
+        return DISTANCES.contains(a, b) ? DISTANCES.get(a, b) : Byte.MAX_VALUE;
     }
 }
