b43d2b37037fcb9c6745781729eff25e09d71854
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-19 10:17:07 +0100

    Add solution for part 2 of day 19

--- src/main/java/aoc2022/day19/Solution.java
+++ src/main/java/aoc2022/day19/Solution.java
@@ -1,18 +1,17 @@
 package aoc2022.day19;
 
 import aoc2022.input.InputLoader;
-import com.google.common.collect.Lists;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import com.google.common.collect.Sets;
+import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.ToString;
 import one.util.streamex.EntryStream;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public final class Solution {
     public static void main(String[] args) {
@@ -20,116 +19,94 @@ public final class Solution {
                 .map(Blueprint::new)
                 .toList();
 
-        final var maxGeodesList = new ArrayList<Integer>();
-        for (final var blueprint : input) {
-            final var initialState = new State(0, 0, 0, 0, 1, 0, 0, 0);
-            final var states = Lists.newArrayList(initialState);
-            var bestState = initialState;
-            var maxGeodes = 0;
-            for (int minute = 0; minute < 24; ++minute) {
-                final var newStates = new HashSet<State>();
-                for (final var state : states) {
-                    final var newOre = state.getOre() + state.getOreRobots();
-                    final var newClay = state.getClay() + state.getClayRobots();
-                    final var newObsidian = state.getObsidian() + state.getObsidianRobots();
-                    final var newGeode = state.getGeode() + state.getGeodeRobots();
-                    maxGeodes = Math.max(maxGeodes, newGeode);
-                    bestState = maxGeodes == newGeode ? state : bestState;
-
-                    newStates.add(new State(newOre, newClay, newObsidian, newGeode,
-                            state.getOreRobots(), state.getClayRobots(), state.getObsidianRobots(),
-                            state.getGeodeRobots()));
-
-                    final var maxOreCost = Stream.of(blueprint.getOre(), blueprint.getClay(),
-                            blueprint.getObsidianOre(),
-                            blueprint.getGeodeOre()).mapToInt(Integer::valueOf).max().orElseThrow();
-                    if (state.getOreRobots() < maxOreCost) {
-                        if (state.getOre() >= blueprint.getOre()) {
-                            newStates.add(new State(newOre - blueprint.getOre(), newClay, newObsidian, newGeode,
-                                    state.getOreRobots() + 1, state.getClayRobots(), state.getObsidianRobots(),
-                                    state.getGeodeRobots()));
-                        }
-                    }
-
-                    if (state.getClayRobots() < blueprint.getObsidianClay()) {
-                        if (state.getOre() >= blueprint.getClay()) {
-                            newStates.add(new State(newOre - blueprint.getClay(), newClay, newObsidian, newGeode,
-                                    state.getOreRobots(), state.getClayRobots() + 1, state.getObsidianRobots(),
-                                    state.getGeodeRobots()));
-                        }
-                    }
-
-                    if (state.getObsidianRobots() < blueprint.getGeodeObsidian()) {
-                        if (state.getOre() >= blueprint.getObsidianOre() && state.getClay() >= blueprint.getObsidianClay()) {
-                            newStates.add(new State(newOre - blueprint.getObsidianOre(), newClay - blueprint.getObsidianClay(), newObsidian, newGeode,
-                                    state.getOreRobots(), state.getClayRobots(), state.getObsidianRobots() + 1,
-                                    state.getGeodeRobots()));
-                        }
-                    }
-
-                    if (state.getOre() >= blueprint.getGeodeOre() && state.getObsidian() >= blueprint.getGeodeObsidian()) {
-                        newStates.add(new State(newOre - blueprint.getGeodeOre(), newClay, newObsidian - blueprint.getGeodeObsidian(), newGeode,
-                                state.getOreRobots(), state.getClayRobots(), state.getObsidianRobots(),
-                                state.getGeodeRobots() + 1));
-                    }
-                }
-                states.clear();
-                for (final var state : newStates) {
-                    if ((state.getGeodeRobots() * (23 - minute)) + state.getGeode() >= maxGeodes) {
-                        states.add(state);
-                    }
-                }
-                System.out.println(minute + " " + states.size() + " " + maxGeodes + " " + bestState);
-            }
-//            System.out.println(blueprint + " " + states.size() + " " + maxGeodes + " " + bestState);
-            maxGeodesList.add(maxGeodes);
-        }
-        System.out.println(maxGeodesList);
-        final var result1 = EntryStream.of(maxGeodesList)
+        final var result1 = EntryStream.of(getMaxGeodesPerBlueprint(input, 24))
                 .mapKeys(Math::incrementExact)
                 .mapKeyValue(Math::multiplyExact)
                 .mapToInt(Integer::valueOf)
                 .sum();
         System.out.println(result1);
+
+        final var result2 = getMaxGeodesPerBlueprint(input.stream().limit(3).toList(), 32).stream()
+                .reduce(Math::multiplyExact)
+                .orElseThrow();
+        System.out.println(result2);
+    }
+
+    private static List<Integer> getMaxGeodesPerBlueprint(final List<Blueprint> blueprints, final int minutes) {
+        return blueprints.stream().map(blueprint -> getMaxGeodesForBlueprint(blueprint, minutes)).toList();
+    }
+
+    private static int getMaxGeodesForBlueprint(final Blueprint blueprint, final int minutes) {
+        final var initialState = new State(0, 0, 0, 0, 1, 0, 0, 0);
+        final var states = Sets.newHashSet(initialState);
+        var maxGeodes = 0;
+        for (int minute = 0; minute < minutes; ++minute) {
+            final var newStates = new HashSet<State>();
+            for (final var state : states) {
+                final var ore = state.ore() + state.oreRobots();
+                final var clay = state.clay() + state.clayRobots();
+                final var obs = state.obs() + state.obsRobots();
+                final var geode = state.geode() + state.geodeRobots();
+                maxGeodes = Math.max(maxGeodes, geode);
+                newStates.add(new State(ore, clay, obs, geode, state.oreRobots(), state.clayRobots(), state.obsRobots(),
+                        state.geodeRobots()));
+                final var maxOreCost = Collections.max(
+                        List.of(blueprint.getOre(), blueprint.getClay(), blueprint.getObsOre(),
+                                blueprint.getGeodeOre()));
+                if (state.oreRobots() < maxOreCost && state.ore() >= blueprint.getOre()) {
+                    newStates.add(new State(ore - blueprint.getOre(), clay, obs, geode, state.oreRobots() + 1,
+                            state.clayRobots(), state.obsRobots(), state.geodeRobots()));
+                }
+                if (state.clayRobots() < blueprint.getObsClay() && state.ore() >= blueprint.getClay()) {
+                    newStates.add(new State(ore - blueprint.getClay(), clay, obs, geode, state.oreRobots(),
+                            state.clayRobots() + 1, state.obsRobots(), state.geodeRobots()));
+                }
+                if (state.obsRobots() < blueprint.getGeodeObs() && state.ore() >= blueprint.getObsOre() &&
+                        state.clay() >= blueprint.getObsClay()) {
+                    newStates.add(new State(ore - blueprint.getObsOre(), clay - blueprint.getObsClay(), obs, geode,
+                            state.oreRobots(), state.clayRobots(), state.obsRobots() + 1, state.geodeRobots()));
+                }
+                if (state.ore() >= blueprint.getGeodeOre() && state.obs() >= blueprint.getGeodeObs()) {
+                    newStates.add(new State(ore - blueprint.getGeodeOre(), clay, obs - blueprint.getGeodeObs(), geode,
+                            state.oreRobots(), state.clayRobots(), state.obsRobots(), state.geodeRobots() + 1));
+                }
+            }
+            states.clear();
+            newStates.stream().filter(stateCanBePruned(minutes, minute, maxGeodes)).forEach(states::add);
+        }
+        return maxGeodes;
+    }
+
+    private static Predicate<State> stateCanBePruned(final int minutes, final int minute, final int maxGeodes) {
+        return state -> (state.geodeRobots() * (minutes - minute)) + state.geode() >= maxGeodes;
     }
 }
 
-@Data
+@Getter(AccessLevel.PACKAGE)
 final class Blueprint {
     private static final Pattern INPUT_PATTERN = Pattern.compile("(\\d+)");
     private final int id;
     private final int ore;
     private final int clay;
-    private final int obsidianOre;
-    private final int obsidianClay;
+    private final int obsOre;
+    private final int obsClay;
     private final int geodeOre;
-    private final int geodeObsidian;
+    private final int geodeObs;
 
     Blueprint(final String raw) {
         final var matcher = INPUT_PATTERN.matcher(raw);
         id = getInt(matcher);
         ore = getInt(matcher);
         clay = getInt(matcher);
-        obsidianOre = getInt(matcher);
-        obsidianClay = getInt(matcher);
+        obsOre = getInt(matcher);
+        obsClay = getInt(matcher);
         geodeOre = getInt(matcher);
-        geodeObsidian = getInt(matcher);
+        geodeObs = getInt(matcher);
     }
 
     private int getInt(final Matcher matcher) {
-        matcher.find();
-        return Integer.parseInt(matcher.group());
+        return matcher.find() ? Integer.parseInt(matcher.group()) : -1;
     }
 }
 
-@Data
-final class State {
-    private final int ore;
-    private final int clay;
-    private final int obsidian;
-    private final int geode;
-    private final int oreRobots;
-    private final int clayRobots;
-    private final int obsidianRobots;
-    private final int geodeRobots;
-}
+record State(int ore, int clay, int obs, int geode, int oreRobots, int clayRobots, int obsRobots, int geodeRobots) { }
