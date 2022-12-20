304bf151402f0686683836ce9e495a950c2e4761
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-20 10:26:38 +0100

    Use a simpler approach for day 20

--- src/main/java/aoc2022/day20/Solution.java
+++ src/main/java/aoc2022/day20/Solution.java
@@ -2,50 +2,50 @@ package aoc2022.day20;
 
 import aoc2022.input.InputLoader;
 import one.util.streamex.EntryStream;
+import one.util.streamex.IntStreamEx;
+import one.util.streamex.LongStreamEx;
 import one.util.streamex.StreamEx;
-import org.javatuples.Pair;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 public final class Solution {
     public static void main(String[] args) {
-        final var input = EntryStream.of(InputLoader.readLines("day20", "input"))
-                .mapValues(Long::parseLong)
-                .mapKeyValue(Pair::with)
-                .toList();
+        final var input = InputLoader.readLines("day20").stream().map(Integer::parseInt).toList();
 
-        final var result1 = solve(input, 1, 1);
+        final var result1 = solve(input, 1L, 1);
         System.out.println(result1);
 
-        final var result2 = solve(input, 811589153, 10);
+        final var result2 = solve(input, 811589153L, 10);
         System.out.println(result2);
     }
 
-    private static long solve(final List<Pair<Integer, Long>> input, final int multiplier, final int mixes) {
-        final var multipliedInput = StreamEx.of(input)
-                .mapToEntry(Pair::getValue0, Pair::getValue1)
+    private static long solve(final List<Integer> input, final long multiplier, final int mixes) {
+        final var keyedInput = EntryStream.of(input)
                 .mapValues(value -> value * multiplier)
-                .mapKeyValue(Pair::with)
+                .mapKeyValue(Number::new)
                 .toMutableList();
-        for (int mix = 0; mix < mixes; ++mix) {
-            for (int i = 0; i < multipliedInput.size(); ++i) {
-                for (int j = 0; j < multipliedInput.size(); ++j) {
-                    if (multipliedInput.get(j).getValue0() == i) {
-                        final var curr = multipliedInput.remove(j);
-                        final var newIndex = Math.floorMod(j + curr.getValue1(), multipliedInput.size());
-                        multipliedInput.add(newIndex, Pair.with(i, curr.getValue1()));
-                        break;
-                    }
-                }
-            }
-        }
-        final var zeroIndex = multipliedInput.stream().map(Pair::getValue1).toList().indexOf(0L);
-        assert zeroIndex >= 0;
-        return Stream.of(
-                multipliedInput.get(Math.floorMod(zeroIndex + 1000, input.size())),
-                multipliedInput.get(Math.floorMod(zeroIndex + 2000, input.size())),
-                multipliedInput.get(Math.floorMod(zeroIndex + 3000, input.size()))
-        ).mapToLong(Pair::getValue1).sum();
+        IntStreamEx.range(mixes).forEach(mix -> mixInput(keyedInput));
+        final var zeroIndex = StreamEx.of(keyedInput).map(Number::value).indexOf(value -> value == 0).orElseThrow();
+        return LongStreamEx.rangeClosed(1000, 3000, 1000)
+                .map(i -> i + zeroIndex)
+                .mapToInt(i -> Math.floorMod(i, input.size()))
+                .mapToObj(keyedInput::get)
+                .mapToLong(Number::value)
+                .sum();
     }
+
+    private static void mixInput(final List<Number> input) {
+        for (int originalIndex = 0; originalIndex < input.size(); ++originalIndex) {
+            final var currentIndex = getCurrentIndex(input, originalIndex);
+            final var number = input.remove(currentIndex);
+            final var newIndex = Math.floorMod(currentIndex + number.value(), input.size());
+            input.add(newIndex, number);
+        }
+    }
+
+    private static int getCurrentIndex(final List<Number> input, final int originalIndex) {
+        return (int) StreamEx.of(input).indexOf(number -> number.originalIndex() == originalIndex).orElseThrow();
+    }
+
+    private record Number(int originalIndex, long value) { }
 }
