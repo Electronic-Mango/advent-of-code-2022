d460a2b878ed716267d29b7ae2b9a0282cec7977
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-24 13:19:38 +0100

    Speed up solution for day 23

--- src/main/java/aoc2022/day24/Solution.java
+++ src/main/java/aoc2022/day24/Solution.java
@@ -1,117 +1,97 @@
 package aoc2022.day24;
 
 import aoc2022.input.InputLoader;
+import com.google.common.collect.Sets;
 import com.google.common.primitives.Chars;
 import one.util.streamex.EntryStream;
+import one.util.streamex.IntStreamEx;
 import one.util.streamex.StreamEx;
 
 import java.awt.Point;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class Solution {
-    private static final Set<Point> DELTAS = Set.of(new Point(0, -1), new Point(0, 1), new Point(-1, 0),
-            new Point(1, 0), new Point(0, 0));
+    private static final Set<Point> DELTAS = Set.of(
+            new Point(0, -1),
+            new Point(0, 1),
+            new Point(-1, 0),
+            new Point(1, 0),
+            new Point(0, 0));
 
     public static void main(final String[] args) {
-        final var input = EntryStream.of(InputLoader.readLines("day24", "input"))
-                .flatMapValues(line -> EntryStream.of(Chars.asList(line.toCharArray())).filterValues(c -> c != '.'))
-                .mapToKey((y, entry) -> new Point(entry.getKey(), y))
-                .mapValues(Map.Entry::getValue)
-                .mapValues(Type::parse)
-                .mapKeyValue(Element::new)
-                .toList();
-        final var maxX = getMaxValue(input, Point::getX);
-        final var maxY = getMaxValue(input, Point::getY);
-        final var start = new Point(1, 0);
-        final var finish = new Point(maxX - 1, maxY);
-        final var states = new LinkedList<Point>();
-        states.add(start);
+        final var input = InputLoader.readLines("day24");
+        final var y = input.size() - 3;
+        final var x = input.stream().mapToInt(String::length).max().orElseThrow() - 3;
+        final var map = prepareMap(input, y);
+        final var start = new Point(0, -1);
+        final var end = new Point(x, y + 1);
+        final var moves = preparePossibleMoves(x, y, start, end);
 
-        final var result1 = move(start, finish, states, input);
+        final var result1 = move(start, end, map, moves, x, y);
         System.out.println(result1);
 
-        final var result2 = result1 + move(finish, start, states, input) + move(start, finish, states, input);
+        final var result2 = result1 + move(end, start, map, moves, x, y) + move(start, end, map, moves, x, y);
         System.out.println(result2);
     }
 
-    private static int move(final Point start, final Point end, final Deque<Point> states, final List<Element> map) {
-        var i = 0;
-        final var maxX = getMaxValue(map, Point::getX);
-        final var maxY = getMaxValue(map, Point::getY);
-        states.clear();
-        states.add(start);
+    private static Map<Character, List<Point>> prepareMap(final List<String> input, final int rows) {
+        return EntryStream.of(input)
+                .skip(1)
+                .limit(rows + 1)
+                .mapValues(line -> line.replace("#", ""))
+                .flatMapValues(line -> EntryStream.of(Chars.asList(line.toCharArray())).filterValues(c -> c >= '<'))
+                .mapToKey((y, entry) -> new Point(entry.getKey(), y - 1))
+                .mapValues(Map.Entry::getValue)
+                .mapKeyValue(Map::entry)
+                .mapToEntry(Map.Entry::getValue, Map.Entry::getKey)
+                .grouping();
+    }
+
+    private static Set<Point> preparePossibleMoves(final int x, final int y, final Point start, final Point end) {
+        return IntStreamEx.rangeClosed(0, x)
+                .boxed()
+                .mapToEntry(Function.identity(), i -> IntStreamEx.rangeClosed(0, y))
+                .flatMapValues(IntStreamEx::boxed)
+                .mapKeyValue(Point::new)
+                .append(start, end)
+                .toSet();
+    }
+
+    private static int move(final Point start,
+                            final Point end,
+                            final Map<Character, List<Point>> map,
+                            final Set<Point> allMoves,
+                            final int x,
+                            final int y) {
+        int i = 0;
+        Set<Point> states = Sets.newHashSet(start);
         while (states.stream().noneMatch(e -> e.equals(end))) {
-            final var newStates = new HashSet<Point>();
-            map.forEach(element -> moveBlizzard(element, maxX, maxY));
-            while (!states.isEmpty()) {
-                final var state = states.pop();
-                final var nextPlayerStates = movePlayer(state, StreamEx.of(map)
-                        .map(Element::position)
-                        .toSet(), maxX, maxY);
-                newStates.addAll(nextPlayerStates);
-            }
             i++;
-            states.addAll(newStates);
+            map.forEach((type, positions) -> moveBlizzard(type, positions, x, y));
+            states = getStates(StreamEx.of(map.values()).flatMap(List::stream).toSet(), allMoves, states);
         }
         return i;
     }
 
-    private static int getMaxValue(final List<Element> element, final Function<Point, Double> getter) {
-        return element.stream().map(Element::position).map(getter).mapToInt(Double::intValue).max().orElseThrow();
+    private static Set<Point> getStates(final Set<Point> blizzards, final Set<Point> moves, final Set<Point> states) {
+        final var currentMoves = Sets.difference(moves, blizzards);
+        return StreamEx.of(states).flatMap(state -> nextMoves(state, currentMoves)).toSet();
     }
 
-    private static void moveBlizzard(final Element element, final int maxX, final int maxY) {
-        if (element.type.equals(Type.EDGE)) {
-            return;
-        }
-        switch (element.type) {
-            case UP -> element.position.translate(0, -1);
-            case DOWN -> element.position.translate(0, 1);
-            case LEFT -> element.position.translate(-1, 0);
-            case RIGHT -> element.position.translate(1, 0);
-        }
-        if (element.position.x >= maxX) {
-            element.position.move(1, element.position.y);
-        } else if (element.position.x < 1) {
-            element.position.move(maxX - 1, element.position.y);
-        } else if (element.position.y >= maxY) {
-            element.position.move(element.position.x, 1);
-        } else if (element.position.y < 1) {
-            element.position.move(element.position.x, maxY - 1);
+    private static void moveBlizzard(final char type, final List<Point> blizzards, final int x, final int y) {
+        switch (type) {
+            case '^' -> blizzards.forEach(p -> p.translate(0, p.y > 0 ? -1 : y));
+            case 'v' -> blizzards.forEach(p -> p.translate(0, p.y < y ? 1 : -y));
+            case '<' -> blizzards.forEach(p -> p.translate(p.x > 0 ? -1 : x, 0));
+            case '>' -> blizzards.forEach(p -> p.translate(p.x < x ? 1 : -x, 0));
         }
     }
 
-    private static Set<Point> movePlayer(final Point expedition, final Set<Point> map, final int maxX, final int maxY) {
-        return DELTAS.stream()
-                .map(delta -> new Point(delta.x + expedition.x, delta.y + expedition.y))
-                .filter(p -> map.stream().noneMatch(o -> o.equals(p)))
-                .filter(p -> p.x >= 0 && p.y >= 0)
-                .filter(p -> p.x <= maxX && p.y <= maxY)
-                .collect(Collectors.toSet());
-    }
-
-    private enum Type {
-        UP, DOWN, LEFT, RIGHT, EDGE;
-
-        static Type parse(final char type) {
-            return switch (type) {
-                case '#' -> EDGE;
-                case '^' -> UP;
-                case 'v' -> DOWN;
-                case '<' -> LEFT;
-                case '>' -> RIGHT;
-                default -> throw new IllegalArgumentException("Unexpected type: " + type);
-            };
-        }
-    }
-
-    private record Element(Point position, Type type) {
+    private static Stream<Point> nextMoves(final Point state, final Set<Point> moves) {
+        return StreamEx.of(DELTAS).map(d -> new Point(d.x + state.x, d.y + state.y)).filter(moves::contains);
     }
 }
