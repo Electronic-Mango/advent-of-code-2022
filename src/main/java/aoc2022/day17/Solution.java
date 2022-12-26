5a637a5a8fe2fd6f87c40409dcecba812f6fe465
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-12-26 11:11:50 +0100

    Add solution for day 17 part 2

--- src/main/java/aoc2022/day17/Solution.java
+++ src/main/java/aoc2022/day17/Solution.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import one.util.streamex.StreamEx;
 
 import java.awt.Point;
 import java.util.HashSet;
@@ -12,6 +13,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.function.IntBinaryOperator;
 
 public final class Solution {
     private static final List<Function<Integer, Shape>> SHAPES = List.of(
@@ -24,22 +26,50 @@ public final class Solution {
     private static final Iterator<Function<Integer, Shape>> SHAPES_CYCLE = Iterables.cycle(SHAPES).iterator();
 
     public static void main(String[] args) {
-        final var directions = InputLoader.read("day17").chars().boxed().toList();
+        final var directions = InputLoader.read("day17", "input").chars().boxed().toList();
         final var directionCycle = Iterables.cycle(directions).iterator();
         final var board = new Board();
         board.add(new Shape(new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(3, 0), new Point(4, 0),
                 new Point(5, 0), new Point(6, 0)));
 
         part1(directionCycle, board, 2022);
-        var result1 = board.height();
+        var result1 = board.top();
         System.out.println(result1);
+
+        final var cycle = part2(directionCycle, board);
+        final var repeating = 1_000_000_000_000L - 2022;
+        final var cycleCount = (repeating / cycle.getShapes().size());
+        final var mod = repeating % cycleCount;
+        part1(directionCycle, board, mod);
+        final var result2 = board.top() + ((cycleCount - 2) * (cycle.top() - cycle.bottom()));
+        System.out.println(result2);
     }
 
-    private static void part1(final Iterator<Integer> directions, final Board board, final int iterations) {
-        for (int i = 0; i < iterations; ++i) {
-            final var newShape = fall(directions, board.getSolidPoints());
+    private static void part1(final Iterator<Integer> directions, final Board board, final long iterations) {
+        for (long i = 0; i < iterations; ++i) {
+            final var newShape = fall(directions, board.getPoints());
             board.add(newShape);
         }
+    }
+
+    private static Board part2(final Iterator<Integer> direction, final Board board) {
+        final var cycle1 = new Board();
+        final var cycle2 = new Board();
+        final var firstShape = fall(direction, board.getPoints());
+        board.add(firstShape);
+        cycle1.add(firstShape);
+        while (!Solution.cycle(cycle1, cycle2)) {
+            final var shape = fall(direction, board.getPoints());
+            board.add(shape);
+            cycle2.add(shape);
+            final var shapeShifted = new Shape(shape);
+            shapeShifted.move(0, cycle1.bottom() - cycle2.bottom());
+            if (!cycle1.getPoints().containsAll(shapeShifted.getPoints())) {
+                cycle2.getShapes().forEach(cycle1::add);
+                cycle2.clear();
+            }
+        }
+        return cycle1;
     }
 
     private static Shape fall(final Iterator<Integer> direction, final Set<Point> solidPoints) {
@@ -52,21 +82,39 @@ public final class Solution {
         }
         return shape;
     }
+
+    private static boolean cycle(final Board b1, final Board b2) {
+        final var offset = b2.bottom() - b1.bottom();
+        return StreamEx.of(b2.getPoints()).map(p -> new Point(p.x, p.y - offset)).toSet().equals(b1.getPoints());
+    }
 }
 
 @EqualsAndHashCode
 @Getter
 final class Board {
     private final Set<Shape> shapes = new HashSet<>();
-    private final Set<Point> solidPoints = new HashSet<>();
+    private final Set<Point> points = new HashSet<>();
 
     void add(final Shape shape) {
         shapes.add(shape);
-        solidPoints.addAll(shape.getPoints());
+        points.addAll(shape.getPoints());
     }
 
-    int height() {
-        return getSolidPoints().stream().mapToInt(p -> p.y).max().orElse(0);
+    void clear() {
+        shapes.clear();
+        points.clear();
+    }
+
+    int top() {
+        return edge(Integer::max);
+    }
+
+    int bottom() {
+        return edge(Integer::min);
+    }
+
+    private int edge(final IntBinaryOperator reduce) {
+        return points.stream().mapToInt(p -> p.y).reduce(reduce).orElse(0);
     }
 }
 
@@ -81,10 +129,14 @@ final class Shape {
         this.points = Sets.newHashSet(points);
     }
 
+    Shape(final Shape shape) {
+        this.points = StreamEx.of(shape.points).map(Point::new).toSet();
+    }
+
     void moveHorizontal(final int direction, final Set<Point> solidPoints) {
         final var dx = direction == '>' ? 1 : -1;
         move(dx, 0);
-        if (getPoints().stream().anyMatch(p -> solidPoints.contains(p) || p.x > MAX_X || p.x < MIN_X)) {
+        if (points.stream().anyMatch(p -> solidPoints.contains(p) || p.x > MAX_X || p.x < MIN_X)) {
             move(-dx, 0);
         }
     }
@@ -100,7 +152,7 @@ final class Shape {
         return atRest;
     }
 
-    private void move(final int dx, final int dy) {
+    void move(final int dx, final int dy) {
         points.forEach(p -> p.translate(dx, dy));
     }
 }
